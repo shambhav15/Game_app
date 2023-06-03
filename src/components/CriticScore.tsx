6c0f4da42d8ca7d8eaf68439c4e79040c7035288
shmbhav <shambhavse15@gmail.com>
2023-06-03 20:01:25 +0530

    optimized image URL

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -1,12 +1,12 @@
-import { Badge } from "@chakra-ui/react"; 
- 
- interface Props{
-    score: number;
- }
- 
- const CriticScore = ({score}: Props) => {
+import { Badge } from "@chakra-ui/react";
 
-    let color = score > 75? 'green' : score > 60? 'yellow' :''; 
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+
+  let color = score > 75 ? 'green' : score > 60 ? 'yellow' : '';
   return (
     <Badge colorScheme={color} fontSize='13px' borderRadius='4px' padding={1}>{score}</Badge>
   )
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames"
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedUrl from "../services/Image-url";
 interface Props {
     game: Game;
 }
@@ -11,7 +12,7 @@ const GameCard = ({ game }: Props) => {
     return (
         <>
             <Card borderRadius={10} overflow={'hidden'}>
-                <Image src={game.background_image} />
+                <Image src={getCroppedUrl(game.background_image)}/>
                 <CardBody>
                     <Heading fontSize='xl'>{game.name}</Heading>
                     <HStack justifyContent={'space-between'}>
